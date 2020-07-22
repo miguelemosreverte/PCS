c2766575b9015d78fe6702152caabd1886cad18a
miguelemosreverte <miguelemosreverte@gmail.com>
2020-07-22 20:30:59 +0000

    4- InfrastructureTestkit => not being used (remove or use) 5- E2ESpec => not being used (remove or use)

--- pcs/common/src/test/scala/design_principles/actor_model/testsuite/ActorAcceptanceTestSuite.scala
+++ pcs/common/src/test/scala/design_principles/actor_model/testsuite/ActorAcceptanceTestSuite.scala
@@ -1,9 +1,9 @@
 package design_principles.actor_model.testsuite
 
 import design_principles.actor_model.{ActorSpec, ActorSpecWriteside}
-import design_principles.actor_model.testkit.{InfrastructureTestkit, QueryTestkit}
+import design_principles.actor_model.testkit.QueryTestkit
 
-trait ActorAcceptanceTestSuite extends ActorSpec with ActorSpecWriteside with InfrastructureTestkit {
+trait ActorAcceptanceTestSuite extends ActorSpec with ActorSpecWriteside {
 
   override val Query: QueryTestkit.AgainstActors
 }

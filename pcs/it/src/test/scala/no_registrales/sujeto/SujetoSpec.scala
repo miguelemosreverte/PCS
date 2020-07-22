922bdb9279b035f479b4246fc1d7ba8a68087d6e
miguelemosreverte <miguelemosreverte@gmail.com>
2020-07-22 21:11:39 +0000

    FIXED it/SujetoSpec

--- pcs/it/src/test/scala/no_registrales/sujeto/SujetoSpec.scala
+++ pcs/it/src/test/scala/no_registrales/sujeto/SujetoSpec.scala
@@ -7,7 +7,7 @@ import no_registrales.BaseE2ESpec
 import infrastructure.cassandra.CassandraTestkit._
 import spec.testkit.ProjectionTestkit
 
-trait SujetoSpec extends consumers_spec.no_registrales.sujeto.SujetoSpec with BaseE2ESpec {
+trait SujetoSpec extends BaseE2ESpec {
 
   implicit val tableName: TableName = TableName("read_side.buc_sujeto")
 
@@ -18,10 +18,15 @@ trait SujetoSpec extends consumers_spec.no_registrales.sujeto.SujetoSpec with Ba
   "sending an obligacion" should "reflect on the api and the database" in parallelActorSystemRunner { implicit s =>
     val context = testContext()
 
-    val obligacion = examples.obligacionWithSaldo50
+
+    val obligacion = stubs.consumers.no_registrales.obligacion.ObligacionExternalDtoStub.obligacionesTri.copy(
+        BOB_SALDO = 50
+    )
     context.messageProducer produceObligacion obligacion
-    val response = context.Query getStateObligacion obligacion
-    response.saldo should be(obligacion.BOB_SALDO)
+    eventually {
+        val response = context.Query getStateObligacion obligacion
+        response.saldo should be(obligacion.BOB_SALDO)
+    }
     (ProjectionTestkit(context) read obligacion) =========================
       Map(
         "suj_identificador" -> obligacion.BOB_SUJ_IDENTIFICADOR,

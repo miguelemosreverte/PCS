96477e2ccb8c75f38bdc9d2861af854c46ea2486
miguelemosreverte <miguelemosreverte@gmail.com>
2020-07-23 20:28:40 +0000

    https://github.com/miguelemosreverte/PCS/pull/1#discussion_r459150885

--- pcs/common/src/test/scala/design_principles/actor_model/system_parallelizable/ActorSystemParallelizerBuilder.scala
+++ pcs/common/src/test/scala/design_principles/actor_model/system_parallelizable/ActorSystemParallelizerBuilder.scala
@@ -7,8 +7,9 @@ import design_principles.actor_model.utils.Generators
 
 object ActorSystemParallelizerBuilder {
 
-  private val system = Generators.actorSystem(new ServerSocket(0).getLocalPort, "ActorSystemParallelizerBuilder")
-  private val actorSystemNumbers = 3551 to 3555
+  def availablePort = new ServerSocket(0).getLocalPort
+  private val system = Generators.actorSystem(availablePort, "ActorSystemParallelizerBuilder")
+  private val actorSystemNumbers: Seq[Int] = (1 to 5) map ( _ => availablePort)
   lazy val actor: ActorRef =
     system.actorOf(ActorSystemParallelizer.props(actorSystemNumbers), s"acceptance-test-actor")
 }

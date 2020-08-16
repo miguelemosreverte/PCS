16ffda17ba5a7c6b6e2be83788aeaa5ea04c4eb6
miguelemosreverte <miguelemosreverte@gmail.com>
2020-08-16 03:22:45 -0300

    Continue to increase parallelism

--- assets/aws/starh.sh
+++ assets/aws/starh.sh
@@ -1,10 +1,9 @@
 kubectl port-forward pod/prometheus-prometheus-prometheus-oper-prometheus-0 9090 &
 kubectl port-forward pod/prometheus-grafana-646f8b887d-j9jwr 3000 &
 
-kubectl port-forward pod/pcs-6d4849b8df-flspl 8086:8081
-kubectl port-forward pod/pcs-6d4849b8df-vtszn 8087:8081
-kubectl port-forward pod/pcs-6d4849b8df-vztv8 8088:8081
-
+kubectl port-forward pod/pcs-6d4849b8df-6jh6m 8086:8081
+kubectl port-forward pod/pcs-6d4849b8df-ch7n8 8087:8081
+kubectl port-forward pod/pcs-6d4849b8df-j6gkv 8088:8081
 
 curl -X POST http://0.0.0.0:8086/kafka/start/DGR-COP-SUJETO-TRI
 curl -X POST http://0.0.0.0:8087/kafka/start/DGR-COP-SUJETO-TRI

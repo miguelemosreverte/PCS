30e9bc913ab04bc4543b0519207f3e46584854d8
miguelemosreverte <miguelemosreverte@gmail.com>
2020-08-29 22:22:41 -0300

    Reduce mapAsync(512)

---
+++ assets/aws/first.sh
@@ -0,0 +1,3 @@
+sh assets/aws/get_inside_admin_pod.sh < assets/aws/install.sh
+
+sh assets/aws/get_inside_admin_pod.sh
--- common/src/main/scala/kafka/KafkaTransactionalMessageProcessor.scala
+++ common/src/main/scala/kafka/KafkaTransactionalMessageProcessor.scala
@@ -51,8 +51,7 @@ class KafkaTransactionalMessageProcessor(
     val `300k_a_minute_per_node` = 300000
     val stream = Transactional
       .source(consumer, subscription)
-      .throttle(`300k_a_minute_per_node`, 1 minute)
-      .mapAsync(1024) { msg: ConsumerMessage.TransactionalMessage[String, String] =>
+      .mapAsync(512) { msg: ConsumerMessage.TransactionalMessage[String, String] =>
         val message = msg
 
         val input: String = message.record.value

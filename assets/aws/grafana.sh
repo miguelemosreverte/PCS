5970851ed8a6860ebf09ac7651c0dfc06eea1d73
miguelemosreverte <miguelemosreverte@gmail.com>
2020-08-30 21:54:39 -0300

    AUTOMATED GRAFANA

--- assets/aws/grafana.sh
+++ assets/aws/grafana.sh
@@ -1,11 +1,16 @@
-curl -X POST -H "Content-Type: application/json" -d '{"admin":"prom-operator"}' http://admin:prom-operator@localhost:3000/api/orgs
-export AUTH9=$(curl -X POST -H "Content-Type: application/json" -d '{"name":"apikeycurl9", "role": "Admin"}' http://admin:prom-operator@localhost:3000/api/auth/keys)
-export KEY9=$(echo $AUTH9 | jq '.key')
-echo $AUTH9
-echo $KEY9
+#!/bin/bash
+
+# this forces usage of bash because $RANDOM is a bash-only variable
+# in case this script is called using sh, it will re-call itself using bash.
+if [ ! "$BASH_VERSION" ] ; then
+    exec /bin/bash "$0" "$@"
+fi
 
 createDashboard() {
-  curl -X POST --insecure -H "Authorization: Bearer $KEY9" -H "Content-Type: application/json" -d '{
+  curl -X POST -H "Content-Type: application/json" -d '{"admin":"prom-operator"}' http://admin:prom-operator@localhost:3000/api/orgs
+  local AUTH22=$(curl -X POST -H "Content-Type: application/json" -d '{"name":"'"$RANDOM"'", "role": "Admin"}' http://admin:prom-operator@localhost:3000/api/auth/keys)
+  local KEY22=$(echo $AUTH22 | jq -r '.key')
+  curl -X POST --insecure -H "Authorization: Bearer $KEY22" -H "Content-Type: application/json" -d '{
     "dashboard": '"$(cat $1)"',
     "overwrite": true
   }' http://localhost:3000/api/dashboards/db

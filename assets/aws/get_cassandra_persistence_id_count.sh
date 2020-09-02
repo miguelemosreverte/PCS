e5918e65f3482058878e33cfb6c1e903ad41dec9
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-02 03:35:42 -0300

    Update query script tohit all cassandra pods

--- assets/aws/get_cassandra_persistence_id_count.sh
+++ assets/aws/get_cassandra_persistence_id_count.sh
@@ -1,7 +1,14 @@
-kubectl exec -it \
-$(kubectl get pod -l app=cassandra -o jsonpath='{.items[0].metadata.name}') \
- -- cqlsh -e 'describe tables';
+#!/bin/bash
 
-kubectl exec -it \
-$(kubectl get pod -l app=cassandra -o jsonpath='{.items[0].metadata.name}') \
- -- cqlsh -e 'select count(*) from akka.all_persistence_ids';
+# this forces usage of bash because for loop is bash-only
+# in case this script is called using sh, it will re-call itself using bash.
+if [ ! "$BASH_VERSION" ] ; then
+    exec /bin/bash "$0" "$@"
+fi
+
+ITER=0
+for cassandra_pod_name in $(kubectl get pods -o name --selector app=cassandra ); do
+  ITER=$(expr $ITER + 1)
+  kubectl exec -it $cassandra_pod_name -- cqlsh -e 'select count(*) from akka.all_persistence_ids'
+  kubectl exec -it $cassandra_pod_name -- cqlsh -e 'select count(*) from akka.messages'
+  done

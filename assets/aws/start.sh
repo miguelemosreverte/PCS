fce11409198327197dfd3682e06735b9cf672b5a
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-02 03:14:46 -0300

    3 Cassandra replicas -- with correct setup

--- assets/aws/start.sh
+++ assets/aws/start.sh
@@ -1,5 +1,6 @@
 
 
+git pull origin MergeThisFranco
 git pull origin AWS
 
 git commit -m "m"
@@ -27,38 +28,19 @@ kubectl exec $kafkaPod -- $createSujetoTri
 export createObligacionTri='kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 20 --topic DGR-COP-OBLIGACIONES-TRI'
 kubectl exec $kafkaPod -- $createObligacionTri
 
-export pod_name=$(kubectl get pod --selector app=cassandra | grep cassandra | cut -d' ' -f 1)
+sh assets/aws/setup_cassandra.sh 
 
-# call setup_cassandra.sh
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka.cql
+export REPLICAS=2
+export PARALELLISM=1024
+export IMAGE=099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
+kubectl apply -f assets/k8s/pcs/pcs-rbac.yml
+envsubst < assets/k8s/pcs/pcs-deployment.yml | kubectl apply -f -
+kubectl apply -f assets/k8s/pcs/pcs-service.yml
+kubectl apply -f assets/k8s/pcs/pcs-service-monitor.yml
 
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka_snapshot.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/all_persistence_ids.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/messages.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/metadata.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/snapshots.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_scanning.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_views.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_write_progress.cql
+export kafka_cluster_ip=$(kubectl get svc kafka-internal -ojsonpath='{.spec.clusterIP}')
+sbt 'it/runMain generator.KafkaEventProducer '"$kafka_cluster_ip"':29092 DGR-COP-SUJETO-TRI 1000000 2000000'
+export kafka_cluster_ip=$(kubectl get svc kafka-internal -ojsonpath='{.spec.clusterIP}')
+sbt 'it/runMain generator.KafkaEventProducer '"$kafka_cluster_ip"':29092 DGR-COP-OBLIGACIONES-TRI 1000000 2000000'
 
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/cqrs/keyspaces/akka_projection.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/infrastructure/cqrs/tables/offset_store.cql
 
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/keyspaces/read_side.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_actividades_sujeto.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_contactos.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_declaraciones_juradas.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_domicilios_objeto.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_domicilios_sujeto.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_exenciones.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_juicios.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_obligaciones.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_planes_pago.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_subastas.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_sujeto.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_sujeto_objeto.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_tramites.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_etapas_procesales.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_param_plan.cql
-kubectl exec -i $pod_name cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_param_recargo.cql

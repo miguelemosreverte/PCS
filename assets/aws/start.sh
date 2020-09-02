f9318df107500c15cccbb81e5d4b9ece99568988
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-02 06:35:28 -0300

    3 Cassandra replicas -- with correct setup

--- assets/aws/start.sh
+++ assets/aws/start.sh
@@ -5,22 +5,24 @@ git pull origin AWS
 
 git commit -m "m"
 
-sbt pcs/docker:publishLocal
-docker tag pcs/pcs:1.0 099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
-docker push 099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
-export IMAGE=099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
+# sbt pcs/docker:publishLocal
+# docker tag pcs/pcs:1.0 099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
+# docker push 099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
+# export IMAGE=099925565557.dkr.ecr.us-west-2.amazonaws.com/pcs-akka:latest
 
-kubectl apply -f assets/k8s/infra/kafka.yml
 # kubectl apply -f assets/k8s/infra/cassandra.yml
 helm repo add incubator https://kubernetes-charts-incubator.storage.googleapis.com
 helm install cassandra incubator/cassandra --version 0.15.2 -f assets/k8s/infra/cassandra.values.yml
+
+sh assets/aws/setup_cassandra.sh
+
+kubectl apply -f assets/k8s/infra/kafka.yml
 
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm install prometheus stable/prometheus-operator --namespace copernico
 
-sh assets/aws/setup_cassandra.sh
-
+sleep 60
 
 export kafkaPod=$(kubectl get pod -l "app=kafka" -o jsonpath='{.items[0].metadata.name}')
 export createSujetoTri='kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 20 --topic DGR-COP-SUJETO-TRI'

a8d11577a80e03da702c57a27c558235e2ea5c8c
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-02 22:17:16 -0300

    3 Cassandra replicas -- with correct setup

--- assets/aws/curlStart.sh
+++ assets/aws/curlStart.sh
@@ -10,11 +10,15 @@ tmux send-keys -t 1 "
       kubectl port-forward $(kubectl get pod -l app=pcs-cluster -o jsonpath='{.items[0].metadata.name}') 8081:8081; \
 " Enter
 
-tmux new-window  -n 'port forward akkaPod2 ' \; split-window -d \;
+tmux new-window  -n 'port forward akkaPod2 ' \;
 tmux send-keys -t 1 "
       kubectl port-forward $(kubectl get pod -l app=pcs-cluster -o jsonpath='{.items[1].metadata.name}') 8082:8081; \
 " Enter
-tmux send-keys -t 2 'sh assets/scripts/query_cassandra_table.sh buc_obligaciones' Enter
+
+tmux new-window  -n 'port forward akkaPod3 ' \;
+tmux send-keys -t 1 "
+      kubectl port-forward $(kubectl get pod -l app=pcs-cluster -o jsonpath='{.items[2].metadata.name}') 8083:8081; \
+" Enter
 
 
 tmux new-window  -n 'START ' \; split-window -d \;
@@ -22,8 +26,10 @@ tmux send-keys -t 1 "
       sleep 10; \
       curl -X POST http://0.0.0.0:8081/kafka/start/DGR-COP-SUJETO-TRI; \
       curl -X POST http://0.0.0.0:8082/kafka/start/DGR-COP-SUJETO-TRI; \
+      curl -X POST http://0.0.0.0:8083/kafka/start/DGR-COP-SUJETO-TRI; \
       curl -X POST http://0.0.0.0:8081/kafka/start/DGR-COP-OBLIGACIONES-TRI; \
       curl -X POST http://0.0.0.0:8082/kafka/start/DGR-COP-OBLIGACIONES-TRI; \
+      curl -X POST http://0.0.0.0:8083/kafka/start/DGR-COP-OBLIGACIONES-TRI; \
 " Enter
 tmux send-keys -t 2 'sleep 20; sh assets/aws/grafana.sh' Enter
 

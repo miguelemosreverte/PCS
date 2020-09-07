c17966bf63a6cba1b5d9b8808bf8d106b83ec992
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-07 17:38:28 -0300

    More throttling

--- assets/aws/curlStart.sh
+++ assets/aws/curlStart.sh
@@ -26,8 +26,10 @@ tmux send-keys -t 1 "
       sleep 10; \
       curl -X POST http://0.0.0.0:8081/kafka/start/DGR-COP-SUJETO-TRI; \
       curl -X POST http://0.0.0.0:8082/kafka/start/DGR-COP-SUJETO-TRI; \
+      curl -X POST http://0.0.0.0:8083/kafka/start/DGR-COP-SUJETO-TRI; \
       curl -X POST http://0.0.0.0:8081/kafka/start/DGR-COP-OBLIGACIONES-TRI; \
       curl -X POST http://0.0.0.0:8082/kafka/start/DGR-COP-OBLIGACIONES-TRI; \
+      curl -X POST http://0.0.0.0:8083/kafka/start/DGR-COP-OBLIGACIONES-TRI; \
 " Enter
 tmux send-keys -t 2 'sleep 20; sh assets/aws/grafana.sh' Enter
 

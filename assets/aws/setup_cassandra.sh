bdd21dd97bc4efce68fcf23768e9f72afd1db7e4
miguelemosreverte <miguelemosreverte@gmail.com>
2020-09-08 15:39:23 -0300

    improve Cassandra k8s config -- more RAM

--- assets/aws/setup_cassandra.sh
+++ assets/aws/setup_cassandra.sh
@@ -11,47 +11,47 @@ for cassandra_pod_name in $(kubectl get pods -o name --selector app=cassandra );
   ITER=$(expr $ITER + 1)
 
   checkCassandra() {
-    kubectl exec $cassandra_pod_name -- cqlsh -e 'describe tables' > /dev/null 2>&1
+    kubectl exec $cassandra_pod_name --container cassandra -- cqlsh -e 'describe tables' > /dev/null 2>&1
   }
   while ! checkCassandra; do
       sleep 1
   done
 
   echo "$cassandra_pod_name IS UP"
-  kubectl exec $cassandra_pod_name -- cqlsh -e 'describe tables'
+  kubectl exec $cassandra_pod_name --container cassandra -- cqlsh -e 'describe tables'
 
   sleep 20
 
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka.cql
 
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka_snapshot.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/all_persistence_ids.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/messages.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/metadata.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/snapshots.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_scanning.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_views.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_write_progress.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/keyspaces/akka_snapshot.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/all_persistence_ids.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/messages.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/metadata.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/snapshots.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_scanning.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_views.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/akka/tables/tag_write_progress.cql
 
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/cqrs/keyspaces/akka_projection.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/infrastructure/cqrs/tables/offset_store.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/cqrs/keyspaces/akka_projection.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/infrastructure/cqrs/tables/offset_store.cql
 
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/keyspaces/read_side.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_actividades_sujeto.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_contactos.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_declaraciones_juradas.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_domicilios_objeto.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_domicilios_sujeto.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_exenciones.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_juicios.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_obligaciones.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_planes_pago.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_subastas.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_sujeto.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_sujeto_objeto.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_tramites.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_etapas_procesales.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_param_plan.cql
-  kubectl exec -i $cassandra_pod_name -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_param_recargo.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/keyspaces/read_side.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_actividades_sujeto.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_contactos.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_declaraciones_juradas.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_domicilios_objeto.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_domicilios_sujeto.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_exenciones.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_juicios.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_obligaciones.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_planes_pago.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_subastas.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_sujeto.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_sujeto_objeto.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_tramites.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_etapas_procesales.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_param_plan.cql
+  kubectl exec -i $cassandra_pod_name --container cassandra -- cqlsh < assets/scripts/cassandra/domain/read_side/tables/buc_param_recargo.cql
   done
